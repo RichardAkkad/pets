746492913565e6d44cbc64322a6332769c9206d5
richard <richardakkad@hotmail.com>
2026-03-27 14:55:23 +0000

    first pet commit

---
+++ pets/src/main/java/com/richy/pets/PetsApplication.java
@@ -0,0 +1,16 @@
+package com.richy.pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsApplication.class, args);
+		System.out.println("hello");
+
+
+
+	}
+}
